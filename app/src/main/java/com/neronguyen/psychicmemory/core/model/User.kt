9a0903b5e9820429b4902a9b5a7c466f1b705c4c
neronguyen <hungnd.vnse@gmail.com>
2024-02-28 01:29:20 +0700

    [feat] Introduce FirebaseUtils for simplified access to currentUser and getToken functionality

--- app/src/main/java/com/neronguyen/psychicmemory/core/model/User.kt
+++ app/src/main/java/com/neronguyen/psychicmemory/core/model/User.kt
@@ -1,18 +1,8 @@
 package com.neronguyen.psychicmemory.core.model
 
-import com.google.firebase.auth.FirebaseUser
-
 data class User(
     val id: String,
     val name: String,
     val email: String,
     val photoUrl: String
 )
-
-fun FirebaseUser.toBusinessModel() = User(
-    id = uid,
-    name = displayName ?: "New User",
-    email = email ?: "New Email",
-    photoUrl = photoUrl.toString()
-)
-
