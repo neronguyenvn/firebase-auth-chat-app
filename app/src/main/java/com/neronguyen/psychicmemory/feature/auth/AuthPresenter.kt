4f798922b329a74821534ce33d186a9fdb4509a3
neronguyen <hungnd.vnse@gmail.com>
2024-02-27 19:53:04 +0700

    [fix+ui] Implement simple welcome message; temporarily disable sign-in button during its execution

--- app/src/main/java/com/neronguyen/psychicmemory/feature/auth/AuthPresenter.kt
+++ app/src/main/java/com/neronguyen/psychicmemory/feature/auth/AuthPresenter.kt
@@ -7,13 +7,15 @@ import androidx.activity.result.ActivityResult
 import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import com.neronguyen.psychicmemory.core.firebase.auth.GoogleAuthClient
 import com.neronguyen.psychicmemory.feature.auth.AuthScreen.Event.SignInClicked
 import com.neronguyen.psychicmemory.feature.chatroom.ChatRoomScreen
 import com.slack.circuit.runtime.CircuitContext
 import com.slack.circuit.runtime.Navigator
-import com.slack.circuit.runtime.popUntil
 import com.slack.circuit.runtime.presenter.Presenter
 import com.slack.circuit.runtime.screen.Screen
 import kotlinx.coroutines.CoroutineScope
@@ -24,14 +26,17 @@ class AuthPresenter(
     private val googleAuthClient: GoogleAuthClient,
 ) : Presenter<AuthScreen.State> {
 
+    private var isLoading by mutableStateOf(false)
+
     @Composable
     override fun present(): AuthScreen.State {
         val coroutineScope: CoroutineScope = rememberCoroutineScope()
         val launcher = rememberGoogleAuthLauncher(coroutineScope)
 
-        return AuthScreen.State { event ->
+        return AuthScreen.State(isLoading) { event ->
             when (event) {
                 SignInClicked -> coroutineScope.launch {
+                    isLoading = true
                     val intentSender = googleAuthClient.getSignInIntent()
                     launcher.launch(
                         IntentSenderRequest.Builder(intentSender ?: return@launch).build()
@@ -52,6 +57,7 @@ class AuthPresenter(
                 coroutineScope.launch {
                     googleAuthClient.signInWithIntent(intent = result.data ?: return@launch)
                     navigator.resetRoot(ChatRoomScreen)
+                    isLoading = false
                 }
             }
         }
--- app/src/main/java/com/neronguyen/psychicmemory/feature/auth/AuthScreen.kt
+++ app/src/main/java/com/neronguyen/psychicmemory/feature/auth/AuthScreen.kt
@@ -1,12 +1,16 @@
 package com.neronguyen.psychicmemory.feature.auth
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.slack.circuit.runtime.CircuitUiEvent
 import com.slack.circuit.runtime.CircuitUiState
 import com.slack.circuit.runtime.screen.Screen
@@ -15,7 +19,10 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data object AuthScreen : Screen {
 
-    data class State(val eventSink: (Event) -> Unit) : CircuitUiState
+    data class State(
+        val isLoading: Boolean,
+        val eventSink: (Event) -> Unit
+    ) : CircuitUiState
 
     sealed class Event : CircuitUiEvent {
         data object SignInClicked : Event()
@@ -25,16 +32,16 @@ data object AuthScreen : Screen {
 @Composable
 fun AuthUi(state: AuthScreen.State, modifier: Modifier = Modifier) {
     Box(modifier.fillMaxSize(), Alignment.Center) {
-        SignInButton {
-            state.eventSink(AuthScreen.Event.SignInClicked)
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text("Welcome to Nero Chat App")
+            Spacer(modifier = Modifier.height(8.dp))
+            Button(
+                enabled = !state.isLoading,
+                onClick = {
+                    state.eventSink(AuthScreen.Event.SignInClicked)
+                }) {
+                Text("Sign In")
+            }
         }
     }
 }
-
-@Composable
-private fun SignInButton(onSignInClick: () -> Unit) {
-    Button(onClick = onSignInClick) {
-        Text("Sign In")
-    }
-}
-
