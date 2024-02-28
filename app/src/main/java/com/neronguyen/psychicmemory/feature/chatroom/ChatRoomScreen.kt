b921a46dfc8f6a670a147186d869045b5ca2eeb3
neronguyen <hungnd.vnse@gmail.com>
2024-02-28 20:35:21 +0700

    [ui] Create message card components for current user and other; format timestamp with default options

--- app/src/main/java/com/neronguyen/psychicmemory/feature/chatroom/ChatRoomScreen.kt
+++ app/src/main/java/com/neronguyen/psychicmemory/feature/chatroom/ChatRoomScreen.kt
@@ -1,7 +1,12 @@
 package com.neronguyen.psychicmemory.feature.chatroom
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -14,22 +19,32 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.neronguyen.psychicmemory.core.model.ChatMessage
 import com.neronguyen.psychicmemory.core.model.User
+import com.neronguyen.psychicmemory.core.ui.CurrentUserMessageCard
+import com.neronguyen.psychicmemory.core.ui.OtherMessageCard
 import com.neronguyen.psychicmemory.feature.chatroom.ChatRoomScreen.Event.ConnectSocket
 import com.neronguyen.psychicmemory.feature.chatroom.ChatRoomScreen.Event.InputMessage
 import com.neronguyen.psychicmemory.feature.chatroom.ChatRoomScreen.Event.SignOut
 import com.neronguyen.psychicmemory.feature.chatroom.component.ChattieTopAppBar
 import com.slack.circuit.runtime.CircuitUiState
 import com.slack.circuit.runtime.screen.Screen
+import kotlinx.datetime.toJavaInstant
 import kotlinx.parcelize.Parcelize
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.util.Locale
 
 @Parcelize
 data object ChatRoomScreen : Screen {
 
     data class State(
         val currentUser: User,
-        val messages: List<String>,
+        val messages: List<ChatMessage>,
         val inputMessage: String,
         val eventSink: (Event) -> Unit
     ) : CircuitUiState
@@ -44,6 +59,7 @@ data object ChatRoomScreen : Screen {
 
 @Composable
 fun ChatRoomUi(state: ChatRoomScreen.State, modifier: Modifier = Modifier) {
+
     LaunchedEffect(true) {
         state.eventSink(ConnectSocket)
     }
@@ -57,9 +73,38 @@ fun ChatRoomUi(state: ChatRoomScreen.State, modifier: Modifier = Modifier) {
         }
     ) { padding ->
         Column(Modifier.padding(padding)) {
-            LazyColumn(Modifier.weight(1f), reverseLayout = true) {
-                items(state.messages) { text ->
-                    Text(text = text)
+            LazyColumn(
+                modifier = Modifier.weight(1f),
+                reverseLayout = true,
+                contentPadding = PaddingValues(
+                    start = 20.dp,
+                    end = 16.dp,
+                    top = 16.dp,
+                    bottom = 16.dp
+                ),
+            ) {
+                items(state.messages) { message ->
+                    when (message) {
+                        is ChatMessage.CurrentUserMessage -> CurrentUserMessageCard(message)
+                        is ChatMessage.OtherMessage -> OtherMessageCard(
+                            message,
+                            shouldShowInfo = message.shouldShowInfo
+                        )
+                    }
+                    if (message.shouldShowTimestamp) {
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Box(Modifier.fillMaxWidth()) {
+                            Text(
+                                text = DateTimeFormatter
+                                    .ofLocalizedDateTime(FormatStyle.MEDIUM)
+                                    .withLocale(Locale.getDefault())
+                                    .withZone(ZoneId.systemDefault())
+                                    .format(message.timestamp.toJavaInstant()),
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(8.dp))
                 }
             }
             OutlinedTextField(
